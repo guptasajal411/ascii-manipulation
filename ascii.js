d94441d3642b4d0aaddaf09ef42e96f2268a69e6
Sajal Gupta <70312106+guptasajal411@users.noreply.github.com>
2021-07-13 19:21:46 +0530

    Add logic for converting text to ASCII

--- ascii.js
+++ ascii.js
@@ -5,17 +5,19 @@ const figlet = require("figlet");
 
 app.use(bodyParser.urlencoded({extended: true}));
  
-figlet("Sajal Gupta", function(err, data) {
-    if (err) {
-        console.log('Something went wrong...');
-        console.dir(err);
-        return;
-    }
-    console.log(data)
-});
-
 app.get("/", function(req, res){
     res.sendFile(__dirname + "/index.html");
+})
+
+app.post("/", function(req, res){
+    figlet(req.body.inputText, function(err, data) {
+        if (err) {
+            console.log('Something went wrong...');
+            console.dir(err);
+            return;
+        }
+        console.log(data)
+    });
 })
 
 app.listen(3000, function(){
